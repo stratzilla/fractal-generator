bd7c56895250bd1891bd2d06fae513835c6b8478
Robin Scott <stratzillab@gmail.com>
2019-05-23 20:34:55 -0400

    Update fractal-generator.sh

--- fractal-generator.sh
+++ fractal-generator.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 if [ -f fractal-generator.out ]; then
-	./fractal-generator.out $1
+	rm fractal-generator.out
 else
 	g++ src/main.cpp -o fractal-generator.out -O2 -lGL -lGLU -lglut -lfreeimage -lX11 -std=c++0x
 	./fractal-generator.out $1
+	rm fractal-generator.out
 fi
-	
