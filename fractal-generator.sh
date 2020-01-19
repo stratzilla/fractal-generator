e9f8e8b66e2e3d2bf51db93dac5953688e65ea77
Robin Scott <stratzillab@gmail.com>
2020-01-19 11:22:03 -0500

    Update fractal-generator.sh

--- fractal-generator.sh
+++ fractal-generator.sh
@@ -3,7 +3,7 @@
 if [ -f fractal-generator.out ]; then
 	rm fractal-generator.out
 else
-	g++ src/*.cpp -o fractal-generator.out -O2 -lGL -lGLU -lglut -lfreeimage -lX11 -std=c++0x
+	g++ src/*.cpp -o fractal-generator.out -O2 -lGL -lGLU -lglut -lfreeimage -lX11
 	./fractal-generator.out $1
 	rm fractal-generator.out
 fi
